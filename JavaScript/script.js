642750756cc0d215e2bdaf6d42c2535ae29e08dd
Rafael <rafaelf.machado2004@gmail.com>
2022-05-11 16:50:47 -0300

    0.3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -15,3 +15,20 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+function leiaMais() {
+  var pontos = document.getElementById("pontos");
+  var maisTexto = document.getElementById("mais");
+  var btnLeiaMais = document.getElementById("btnLeiaMais");
+  
+
+  if(pontos.style.display === "none") {
+    pontos.style.display = "inline";
+    maisTexto.style.display = "none";
+    btnLeiaMais.innerHTML = "Leia Mais...";
+  } else {
+    pontos.style.display = "none";
+    maisTexto.style.display = "inline";
+    btnLeiaMais.innerHTML = "Leia Menos";
+  }
+}
